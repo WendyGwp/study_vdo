ebc0b8e129aa8c1707686e92f7f42e25598b4394
WendyGwp <140592241+WendyGwp@users.noreply.github.com>
2023-08-29 13:17:57 +0800

    Update runbatch.py

--- runbatch.py
+++ runbatch.py
@@ -5,6 +5,8 @@ from run import estimate
 import numpy
 import PIL
 import PIL.Image
+import argparse
+
 def generate_optical_flow(input_folder, output_folder):
     # 创建输出文件夹
     os.makedirs(output_folder, exist_ok=True)
@@ -15,35 +17,17 @@ def generate_optical_flow(input_folder, output_folder):
     # 定义图像变换
     transform = transforms.ToTensor()
 
-    tenOne = torch.FloatTensor(numpy.ascontiguousarray(
-        numpy.array(PIL.Image.open(input_folder + "/" + file_list[0]))[:, :, ::-1].transpose(2, 0, 1).astype(
-            numpy.float32) * (
-                1.0 / 255.0)))
-    tenTwo = torch.FloatTensor(numpy.ascontiguousarray(
-        numpy.array(PIL.Image.open(input_folder + "/" + file_list[0]))[:, :, ::-1].transpose(2, 0, 1).astype(
-            numpy.float32) * (
-                1.0 / 255.0)))
-
-    tenOutput = estimate(tenOne, tenTwo)
-
-    objOutput = open(output_folder + '/' + file_list[0][:-4] + '.flo', 'wb')
-
-    numpy.array([80, 73, 69, 72], numpy.uint8).tofile(objOutput)
-    numpy.array([tenOutput.shape[2], tenOutput.shape[1]], numpy.int32).tofile(objOutput)
-    numpy.array(tenOutput.numpy().transpose(1, 2, 0), numpy.float32).tofile(objOutput)
-
-    objOutput.close()
     for i in range(len(file_list) - 1):
-        tenOne = torch.FloatTensor(numpy.ascontiguousarray(
-            numpy.array(PIL.Image.open(input_folder+"/"+file_list[i]))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (
-                        1.0 / 255.0)))
-        tenTwo = torch.FloatTensor(numpy.ascontiguousarray(
-            numpy.array(PIL.Image.open(input_folder+"/"+file_list[i+1]))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (
-                        1.0 / 255.0)))
+        image_one = PIL.Image.open(os.path.join(input_folder, file_list[i])).convert("RGB")
+        image_two = PIL.Image.open(os.path.join(input_folder, file_list[i+1])).convert("RGB")
+
+        tenOne = torch.FloatTensor(numpy.ascontiguousarray(numpy.array(image_one)[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0)))
+        tenTwo = torch.FloatTensor(numpy.ascontiguousarray(numpy.array(image_two)[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0)))
+
 
         tenOutput = estimate(tenOne, tenTwo)
 
-        objOutput = open(output_folder+'/'+file_list[i+1][:-4]+'.flo', 'wb')
+        objOutput = open(os.path.join(output_folder, file_list[i+1][:-4]+'.flo'), 'wb')
 
         numpy.array([80, 73, 69, 72], numpy.uint8).tofile(objOutput)
         numpy.array([tenOutput.shape[2], tenOutput.shape[1]], numpy.int32).tofile(objOutput)
@@ -51,28 +35,13 @@ def generate_optical_flow(input_folder, output_folder):
 
         objOutput.close()
 
-
-        # # 读取当前帧和下一帧图像
-        # current_frame = Image.open(os.path.join(input_folder, file_list[i]))
-        # next_frame = Image.open(os.path.join(input_folder, file_list[i+1]))
-        #
-        # # 进行图像变换
-        # current_frame = transform(current_frame)
-        # next_frame = transform(next_frame)
-        #
-        # # 使用光流估计函数计算光流
-        # flow = estimate(current_frame, next_frame)
-        #
-        # # 生成输出文件路径
-        # flow_output_path = os.path.join(output_folder, f"{file_list[i+1][:-4]}.flo")
-        #
-        # # 将光流保存为文件
-        # torch.save(flow, flow_output_path)
-
         print(f"Generated optical flow for {file_list[i+1]}")
 
-# 示例用法
-input_folder = "./image_0"
-output_folder = "./flow"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate optical flow from image sequence")
+    parser.add_argument("input_folder", type=str, help="Folder containing input images")
+    parser.add_argument("output_folder", type=str, help="Folder to save generated optical flow")
 
-generate_optical_flow(input_folder, output_folder)
+    args = parser.parse_args()
+
+    generate_optical_flow(args.input_folder, args.output_folder)
